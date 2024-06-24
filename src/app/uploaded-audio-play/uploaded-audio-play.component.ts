54359861f79266b68c7e77f484bbe047f7265864
JBorad988 <jayborad366@gmail.com>
2024-06-24 12:23:07 +0530

    initial-commit

---
+++ src/app/uploaded-audio-play/uploaded-audio-play.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uploaded-audio-play',
+  templateUrl: './uploaded-audio-play.component.html',
+  styleUrls: ['./uploaded-audio-play.component.scss']
+})
+export class UploadedAudioPlayComponent {
+
+}
